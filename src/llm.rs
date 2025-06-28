9ac38718037c7634840faa4138d12cd56c3e346a
Andy Salerno <andysalerno@gmail.com>
2025-06-27 18:07:54 -0700

    add usage

--- src/llm.rs
+++ src/llm.rs
@@ -8,7 +8,7 @@ use async_openai::{
     },
 };
 use futures_util::StreamExt;
-use log::{debug, error, info};
+use log::{debug, error};
 use tokio::runtime::Runtime;
 
 pub struct LlmClient {
@@ -20,8 +20,49 @@ pub struct LlmRequest {
     pub text: String,
 }
 
+#[derive(Debug, Clone, Default)]
 pub struct LlmResponse {
-    pub text: String,
+    text: String,
+    usage: Usage,
+}
+
+impl LlmResponse {
+    pub fn text(&self) -> &str {
+        &self.text
+    }
+
+    pub fn usage(&self) -> &Usage {
+        &self.usage
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Usage {
+    prompt_tokens: u32,
+    completion_tokens: u32,
+    total_tokens: u32,
+}
+
+impl Usage {
+    pub fn new(prompt_tokens: u32, completion_tokens: u32, total_tokens: u32) -> Self {
+        Self {
+            prompt_tokens,
+            completion_tokens,
+            total_tokens,
+        }
+    }
+
+    pub fn prompt_tokens(&self) -> u32 {
+        self.prompt_tokens
+    }
+
+    pub fn completion_tokens(&self) -> u32 {
+        self.completion_tokens
+    }
+
+    pub fn total_tokens(&self) -> u32 {
+        self.total_tokens
+    }
 }
 
 static RT: std::sync::LazyLock<Runtime> = std::sync::LazyLock::new(|| {
@@ -47,18 +88,23 @@ impl LlmClient {
         Self { client, model_name }
     }
 
-    pub fn get_chat_completion(
+    pub fn get_chat_completion_streaming(
         &self,
         request: &LlmRequest,
+        mut action_per_chunk: impl FnMut(&str),
     ) -> Result<LlmResponse, Box<dyn std::error::Error>> {
         let request = CreateChatCompletionRequestArgs::default()
             .model(&self.model_name)
             .temperature(0.7)
             .stream(true)
+            .stream_options(ChatCompletionStreamOptions {
+                include_usage: true,
+            })
             .messages(vec![user_message(&request.text)?])
             .build()?;
 
         let mut gradual_response = String::new();
+        let mut usage = Usage::default();
 
         RT.block_on(async {
             let gradual_response = &mut gradual_response;
@@ -72,7 +118,13 @@ impl LlmClient {
                         if let Some(delta) =
                             chunk.choices.first().and_then(|c| c.delta.content.as_ref())
                         {
+                            action_per_chunk(delta);
                             gradual_response.push_str(delta);
+                        }
+
+                        if let Some(u) = chunk.usage {
+                            usage =
+                                Usage::new(u.prompt_tokens, u.completion_tokens, u.total_tokens);
                         }
                     }
                     Err(e) => {
@@ -83,11 +135,19 @@ impl LlmClient {
             }
         });
 
-        info!("Response: {gradual_response}");
+        debug!("Response: {gradual_response}");
 
         Ok(LlmResponse {
             text: gradual_response,
+            usage,
         })
+    }
+
+    pub fn get_chat_completion(
+        &self,
+        request: &LlmRequest,
+    ) -> Result<LlmResponse, Box<dyn std::error::Error>> {
+        self.get_chat_completion_streaming(request, |_| {})
     }
 }
 
--- src/run.rs
+++ src/run.rs
@@ -8,9 +8,15 @@ use crate::{
 pub(crate) fn run(config: Config, input: &str) -> Result<(), Box<dyn std::error::Error>> {
     let llm = llm::LlmClient::new(config.model_name, config.api_key, config.api_url);
 
-    let response = llm.get_chat_completion(&LlmRequest { text: input.into() })?;
+    // let response = llm.get_chat_completion(&LlmRequest { text: input.into() })?;
 
-    info!("LLM Response: {}", response.text);
+    let response =
+        llm.get_chat_completion_streaming(&LlmRequest { text: input.into() }, |chunk| {
+            info!("{chunk}");
+        })?;
+
+    info!("LLM Response: {}", response.text());
+    info!("{:?}", response.usage());
 
     Ok(())
 }
