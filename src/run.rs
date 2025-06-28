c48baab4e6b9c20c1b34bc6b784415591f6a344f
Andy Salerno <andysalerno@gmail.com>
2025-06-27 22:07:21 -0700

    flush stdout after the responses

--- src/run.rs
+++ src/run.rs
@@ -1,21 +1,25 @@
-use log::info;
+use std::io::Write;
+
+use log::{debug, info};
 
 use crate::{
     config::Config,
     llm::{self, LlmRequest},
 };
 
-pub(crate) fn run(config: Config, input: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run(config: Config, input: &str) -> Result<(), Box<dyn std::error::Error>> {
     let llm = llm::LlmClient::new(config.model_name, config.api_key, config.api_url);
-
-    // let response = llm.get_chat_completion(&LlmRequest { text: input.into() })?;
 
     let response =
         llm.get_chat_completion_streaming(&LlmRequest { text: input.into() }, |chunk| {
-            info!("{chunk}");
+            print!("{chunk}");
         })?;
 
-    info!("LLM Response: {}", response.text());
+    println!();
+
+    std::io::stdout().flush()?;
+
+    debug!("LLM Response: {}", response.text());
     info!("{:?}", response.usage());
 
     Ok(())
